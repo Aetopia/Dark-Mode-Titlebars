004b6dab440a8d134570b3065296be5c9ce92bf4
Aetopia <kirbymario22@gmail.com>
2022-12-28 17:30:39 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-nim c -d:release -d:strip -o:"DMTB.exe" src/DMTB.nim  
+nim c --app:gui -d:release -d:strip -o:"DMTB.exe" src/DMTB.nim  
 :: Optional UPX compression.
 upx --best --ultra-brute DMTB.exe>nul 2>&1
